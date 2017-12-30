04d8827380124a0b547500549b44a9d2328fa4cd
Robert Laszczak <eventched@gmail.com>
2017-12-30 19:29:27 +0100

    fixed http2 compatibility in channelEventStream

--- responder.go
+++ responder.go
@@ -146,7 +146,13 @@ func channelEventStream(w http.ResponseWriter, r *http.Request, v interface{}) {
 
 	w.Header().Set("Content-Type", "text/event-stream; charset=utf-8")
 	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
+
+	if r.ProtoMajor == 1 {
+		// An endpoint MUST NOT generate an HTTP/2 message containing connection-specific header fields.
+		// Source: RFC7540
+		w.Header().Set("Connection", "keep-alive")
+	}
+
 	w.WriteHeader(200)
 
 	ctx := r.Context()
